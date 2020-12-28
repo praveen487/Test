6ecd4b58e88d28b57d2be3f3193e853c28e8d2ca
praveen487 <76676824+praveen487@users.noreply.github.com>
2020-12-28 17:50:59 +0530

    Update views.py

--- DemoApp/views.py
+++ DemoApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def indexview(request):
-    return HttpResponse("<h1>hello world</h1>")
+    return HttpResponse("<h2><i>hello world</i></h2> <h1>Something BIG is coming</h1>")
